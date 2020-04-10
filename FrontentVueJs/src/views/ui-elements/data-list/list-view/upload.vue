960a2f210f3ad748b8aaff77e9172e85e7713d61
AliHafeez337 <alihafeez337@gmail.com>
2020-04-10 10:33:25 +0500

    2nd- Omer's changes

--- FrontentVueJs/src/views/ui-elements/data-list/list-view/upload.vue
+++ FrontentVueJs/src/views/ui-elements/data-list/list-view/upload.vue
@@ -10,9 +10,11 @@
           v-validate="'required'"
           name="name"
         />
-        <span class="text-danger text-sm" v-show="errors.has('name')">{{
+        <span class="text-danger text-sm" v-show="errors.has('name')">
+          {{
           errors.first("name")
-        }}</span>
+          }}
+        </span>
       </div>
       <div class="vx-col md:w-1/2 w-full mt-5">
         <vs-select
@@ -30,9 +32,11 @@
             class="w-full"
           />
         </vs-select>
-        <span class="text-danger text-sm" v-show="errors.has('category')">{{
+        <span class="text-danger text-sm" v-show="errors.has('category')">
+          {{
           errors.first("category")
-        }}</span>
+          }}
+        </span>
       </div>
       <div class="vx-col md:w-full w-full mt-4">
         <h3>Please Select Permission:</h3>
@@ -40,23 +44,17 @@
           <div class="vx-row">
             <div class="vx-col md:w-1/3 w-full mt-5">
               <li>
-                <vs-checkbox v-model="file.userAllowed" vs-value="user"
-                  >user</vs-checkbox
-                >
+                <vs-checkbox v-model="file.userAllowed" vs-value="user">user</vs-checkbox>
               </li>
             </div>
             <div class="vx-col md:w-1/3 w-full mt-5">
               <li>
-                <vs-checkbox v-model="file.userAllowed" vs-value="customer"
-                  >customer</vs-checkbox
-                >
+                <vs-checkbox v-model="file.userAllowed" vs-value="customer">customer</vs-checkbox>
               </li>
             </div>
             <div class="vx-col md:w-1/3 w-full mt-5">
               <li>
-                <vs-checkbox v-model="file.userAllowed" vs-value="partner"
-                  >partner</vs-checkbox
-                >
+                <vs-checkbox v-model="file.userAllowed" vs-value="partner">partner</vs-checkbox>
               </li>
             </div>
           </div>
@@ -72,44 +70,28 @@
           v-validate="'required'"
           name="description"
         />
-        <span class="text-danger text-sm" v-show="errors.has('description')">{{
+        <span class="text-danger text-sm" v-show="errors.has('description')">
+          {{
           errors.first("description")
-        }}</span>
+          }}
+        </span>
       </div>
 
       <vs-row vs-align="center" vs-type="flex" vs-justify="flex-end" vs-w="12">
-        <vs-col
-          vs-type="flex"
-          vs-justify="center"
-          vs-align="center"
-          vs-w="4    "
-        >
-          <vs-button color="success" type="gradient" @click="post()"
-            >Next</vs-button
-          >
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="4    ">
+          <vs-button color="success" type="gradient" @click="post()">Next</vs-button>
         </vs-col>
       </vs-row>
     </div>
 
-    <div class="vx-row tab2 " style="display: none;">
+    <div class="vx-row tab2" style="display: none;">
       <vs-row vs-align="center" vs-type="flex" vs-justify="flex-end" vs-w="12">
-        <vs-col
-          vs-type="flex"
-          vs-justify="center"
-          vs-align="center"
-          vs-w="4    "
-        >
-          <vs-button color="danger" type="gradient" @click="redirect"
-            >Redirect</vs-button
-          >
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="4    ">
+          <vs-button color="danger" type="gradient" @click="redirect">Redirect</vs-button>
         </vs-col>
       </vs-row>
       <div class="vx-col md:w-full w-full mt-5">
-        <vs-input
-          label="Enter Language"
-          v-model="fileo.language"
-          class="w-full"
-        />
+        <vs-input label="Enter Language" v-model="fileo.language" class="w-full" />
       </div>
       <div class="vx-col md:w-full w-full mt-5">
         <input
@@ -123,30 +105,18 @@
           @click="javaScr()"
           v-on:change="handleFileUpload()"
         />
-        <label for="file-2"
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="17"
-            viewBox="0 0 20 17"
-          >
+        <label for="file-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17">
             <path
               d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"
             />
           </svg>
-          <span>Choose a file&hellip;</span></label
-        >
+          <span>Choose a file&hellip;</span>
+        </label>
       </div>
       <vs-row vs-align="center" vs-type="flex" vs-justify="flex-end" vs-w="12">
-        <vs-col
-          vs-type="flex"
-          vs-justify="center"
-          vs-align="center"
-          vs-w="4    "
-        >
-          <vs-button color="success" type="gradient" @click="put()"
-            >Submit</vs-button
-          >
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="4    ">
+          <vs-button color="success" type="gradient" @click="put()">Submit</vs-button>
         </vs-col>
       </vs-row>
     </div>
@@ -297,6 +267,7 @@ export default {
     post() {
       this.$validator.validateAll().then(result => {
         if (result) {
+          console.log("array is" + this.file.userAllowed);
           return new Promise((resolve, reject) => {
             axios
               .post("/uploads/", this.file)
