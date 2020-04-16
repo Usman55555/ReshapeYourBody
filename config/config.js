242d395388191bede1c4d0ae19e1848b8a1be3d5
AliHafeez337 <alihafeez337@gmail.com>
2020-04-16 11:37:18 +0500

    30th - last changes, going for 1st job, going to send this project...

--- config/config.js
+++ config/config.js
@@ -5,13 +5,9 @@ module.exports = {
     project: 'Reshape Your Body 👻',
     email: 'reshape.your.body.01@gmail.com',
     password: '0o3Qz@kMnmwBXMFukhrwlixh',
-    // facebook_clientID: '354275715466978',
-    // facebook_clientSecret: '39ad5da18dbfdae5ab124ef5b258ed0d',
     facebook_clientID: '519420778745964',
     facebook_clientSecret: '1df70de0a358efe388f02a8eb809eadb',
     facebook_callbackURL: 'http://localhost:3000/api/user/auth/facebook/callback',
-    // google_clientID: '760587587334-uqkmi253k0q5c9udtm48ttp1j1j5ed00.apps.googleusercontent.com',
-    // google_clientSecret: 'Q4kLt8sXpVL42bFHNEl0WcZf',
     google_clientID: '713840005730-8isttfj29rlsmosscjovbuus6prehvtj.apps.googleusercontent.com',
     google_clientSecret: 'MUN4Xv-7gpXhK_3gImJ1NREL',
     google_callbackURL: "http://localhost:3000/api/user/auth/google/callback"
